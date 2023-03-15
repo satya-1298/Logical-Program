0ec3b6bfca896105a776ae564961fe99191391c4
unknown <satya-1298>
2023-03-15 19:38:36 +0530

    PerfectNumberProblem

---
+++ Logical/Perfect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Perfect
+    {
+        public static void number()
+        {
+            int n, i, sum;
+            int min, max;
+            Console.WriteLine("Finding the Perfect number with in the given range");
+            Console.WriteLine("Input starting range");
+            min=int.Parse(Console.ReadLine());
+            Console.WriteLine("Input ending range");
+            max=int.Parse(Console.ReadLine());
+            for (n=min;n<=max;n++)
+            {
+                i = 1;
+                sum = 0;
+                while (i < n) 
+                {
+                    if (n%i==0)
+                    sum=sum+i;
+                    i++;
+
+                }
+                if (sum == n)
+                    Console.WriteLine(n);
+            }
+        }
+    }
+}
