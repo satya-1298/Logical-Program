0b74640ec035f4c4f5ef7b9b26034996b7d9b1e6
unknown <satya-1298>
2023-03-16 22:53:05 +0530

    Fators of a number

---
+++ Logical/Factors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical
+{
+    public class Factors
+    {
+        public static void ofnumber()
+        {
+            Console.WriteLine("Enter a number");
+            int n=int.Parse(Console.ReadLine());
+           
+            for (int i = 1; i <= n;i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
